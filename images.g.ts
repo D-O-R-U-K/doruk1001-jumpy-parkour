cf40360899240b09d9f6c4441743d0132d32697c
D-O-R-U-K <89851635+D-O-R-U-K@users.noreply.github.com>
2024-08-28 17:57:04 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Drk":return img`
+. . . . . . e e e e e . . . . . 
+. . . . . . e d d d e . . . . . 
+. . . . . . d f d f d . . . . . 
+. . . . . . 2 d d d 2 . . . . . 
+. . . . . . f d d d f . . . . . 
+. . . . . 5 f f f f f 5 . . . . 
+. . . . . 5 5 5 5 5 5 5 . . . . 
+. . . . . 4 f f f 7 7 4 . . . . 
+. . . . . 4 f 7 f 7 7 4 . . . . 
+. . . . . 4 f 7 f 7 7 4 . . . . 
+. . . . . 4 f f f 7 7 4 . . . . 
+. . . . . . 2 2 . 2 2 . . . . . 
+. . . . . . 2 2 . 2 2 . . . . . 
+. . . . . . 2 2 . 2 2 . . . . . 
+. . . . f f f f . f f f f . . . 
+. . . . f f f f . f f f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
